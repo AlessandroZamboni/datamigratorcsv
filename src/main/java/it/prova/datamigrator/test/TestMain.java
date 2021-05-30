64b5f0be00234fc0de23d0cc1463cd4c73fd2855
AlessandroZamboni <62562658+AlessandroZamboni@users.noreply.github.com>
2021-05-31 00:02:07 +0200

    Update TestMain.java

--- src/main/java/it/prova/datamigrator/test/TestMain.java
+++ src/main/java/it/prova/datamigrator/test/TestMain.java
@@ -26,39 +26,8 @@ public class TestMain {
             e.printStackTrace();
         }
 
-        //################# CSV READER TEST ##############
-     /*     String fileName = "C:\\Users\\Alessandro\\Desktop\\filesinput\\assicurato.csv";
-        CSVParser csvParser = new CSVParserBuilder().withSeparator(',').withIgnoreQuotations(true).build(); // custom separator
-        try(CSVReader reader = new CSVReaderBuilder(
-                new FileReader(fileName))
-                .withCSVParser(csvParser)   // custom CSV parser
-                .withSkipLines(1)           // skip the first line, header info
-                .build())
-        {
-            System.out.println("########## ESTRAZIONE #########\n");
-
-            List<String[]> r = reader.readAll();
-          //  r.forEach(x -> System.out.println(Arrays.toString(x)));
-            r.stream().flatMap(Arrays::stream).forEach(System.out::println);
 
 
-            System.out.println("\n########## FINE ESTRAZIONE #########\n");
-
-            for(String[] assicuratoItem : r) {
-
-
-                Assicurato assicurato = new Assicurato();
-                assicurato.setId(Long.parseLong(assicuratoItem[0]));
-                assicurato.setNome(assicuratoItem[1]);
-                assicurato.setCognome(assicuratoItem[2]);
-                assicurato.setDataNascita(Date.valueOf(assicuratoItem[3]));
-                assicurato.setCodiceFiscale(assicuratoItem[4]);
-                assicurato.setNumeroSinistri(Integer.parseInt(assicuratoItem[5]));
-
-                System.out.println("ASSICURATO COSTRUITO: "+assicurato);
-
-            }*/
-
-        }
+     }
 
 }
